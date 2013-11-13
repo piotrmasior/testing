f92b7164a058443b0ea7509a32db869c2005f1e8
Piotr Mąsior <pmasior@railsutils.com>
2013-11-13 17:48:37 +0100

    scafold app 3.2 with 1.9.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testing::Application.config.secret_token = '567da745fc054b16e9a33283ed4e32100d2e98201f656658f8993a2a22f371c216f1047c504fb742263d2325e1da49fb91def71e74d39068c1765365d850405b'
